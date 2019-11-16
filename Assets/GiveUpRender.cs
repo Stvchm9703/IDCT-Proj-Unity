733380b64f7029630c8fb6d194aafa4f11041500
Steven Cheng <steven.cheng@teksbotics.com>
2019-11-16 19:36:46 +0800

    Adjust GUI render

--- Assets/GiveUpRender.cs
+++ Assets/GiveUpRender.cs
@@ -2,33 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GiveUpRender : MonoBehaviour {
+public class GiveUpRender: MonoBehaviour {
     public GiveUpPanel Controller;
     GUIContent btn_gui;
     public GUIStyle default_gui;
     Vector2 position_;
-    void Start () {
+    Vector2 size_bx;
+    void Start() {
         if (!this.Controller) {
-            this.Controller = GameObject.FindWithTag ("Controller").GetComponent<GiveUpPanel> ();
+            this.Controller = GameObject.FindWithTag("Controller").GetComponent<GiveUpPanel>();
         }
-        btn_gui = new GUIContent ();
-        position_.x = Screen.width / 2 - 80;
-        position_.y = this.gameObject.GetComponent<RectTransform> ().sizeDelta.y / 2 + 50;
-        Debug.Log (position_);
+        btn_gui = new GUIContent();
+        float g = 0;
+        if (Screen.width < Screen.height) {
+            g = Screen.width;
+        } else {
+            g = Screen.height;
+        }
+        size_bx.x = (float)(g * 0.3);
+        size_bx.y = (float)(g * 0.1);
+        position_.x = (float)(Screen.width / 2 - size_bx.x/2);
+        position_.y = Screen.height;
+
+        // Debug.Log(position_);
+        default_gui.fontSize = (int)(48);
     }
-    void OnGUI () {
+    void OnGUI() {
         if (this.gameObject.active) {
             GUI.skin.button = default_gui;
-            var t = new GUIContent (btn_gui); //copy
+            var t = new GUIContent(btn_gui); // copy
             t.text = "Quit Game";
-            if (GUI.Button (new Rect (position_.x, position_.y + 30, 160, 45), t)) {
-                this.Controller.giveuponclick ();
+
+            if (GUI.Button(new Rect(position_.x, (float)(position_.y * 0.9), (float)(size_bx.x), (float)(size_bx.y)), t)) {
+                this.Controller.giveuponclick();
             }
 
-            var y = new GUIContent (btn_gui);
+            var y = new GUIContent(btn_gui);
             y.text = "Close";
-            if (GUI.Button (new Rect (position_.x, position_.y + 100, 160, 45), y)) {
-                this.Controller.giveupcloseclick ();
+
+            if (GUI.Button(new Rect(position_.x, (float)(position_.y * 0.8), (float)(size_bx.x), (float)(size_bx.y)), y)) {
+                this.Controller.giveupcloseclick();
             }
         }
     }
