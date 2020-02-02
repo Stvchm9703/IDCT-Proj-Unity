e4ce296d597f04a347923c173c07533d43888ea4
Steven Cheng <stv.chm@gmail.com>
2020-02-02 13:05:25 +0800

    relocate the files

---
+++ Assets/Scripts/AC_CreateForm.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+public class AC_CreateForm : MonoBehaviour {
+    public string address, username, pw_key;
+    public InputField address_f, username_f, pw_key_f;
+    public Animator switcher;
+    public GameObject LoadingPanel;
+    void Start() {
+        if (address_f == null)
+            address_f = this.transform.parent.Find("Canvas/create_part/server_ip").GetComponent<InputField>();
+        if (username_f == null)
+            username_f = this.transform.parent.Find("Canvas/create_part/username").GetComponent<InputField>();
+        if (pw_key_f == null)
+            pw_key_f = this.transform.parent.Find("Canvas/create_part/password").GetComponent<InputField>();
+        if (switcher == null) {
+            switcher = this.transform.parent.Find("Canvas").GetComponent<Animator>();
+        }
+        if (LoadingPanel == null) {
+            LoadingPanel = this.transform.parent.Find("Canvas/LoadingPanel").gameObject;
+        }
+    }
+    public void AddressChecking(string input) {
+
+        Regex ip = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+        MatchCollection result = ip.Matches(input);
+        if (ip.IsMatch(input)) {
+            Debug.Log(result[0]);
+            address = input;
+        } else {
+            address_f.text = "";
+            var text_box = address_f.textComponent.transform.parent.Find("Placeholder").GetComponent<Text>();
+            text_box.text = "Invaild address";
+            text_box.color = new Color(0.8f, 0f, 0f, 0.5f);
+
+        }
+    }
+    public void SwitchToLogin() {
+        switcher.Play("switch_login");
+    }
+}
